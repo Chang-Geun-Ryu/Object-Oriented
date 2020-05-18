07e5a463b475e4a2d98226180392a29f27eb4e46
ryu <goodrcg@gmail.com>
2020-05-18 09:47:54 +0900

    update ass 18

--- Assignment1/src/academy/pocu/comp2500/assignment1/Program.java
+++ Assignment1/src/academy/pocu/comp2500/assignment1/Program.java
@@ -7,28 +7,31 @@ public class Program {
     public static void main(String[] args) {
 	    // write your code here
         BlogHost blog = new BlogHost();
-        Content post = new Content("title1", "body2", "host");
-        Content post2 = new Content("title2", "body1", "host");
+        Content post = new Content("1", "body2", "host");
+        Content post2 = new Content("2", "body1", "host");
         blog.addPost(post);
         blog.addPost(post2);
-        Content post3 = new Content("title3", "body2", "vi");
-        Content post4 = new Content("title4", "body1", "vi");
+        Content post3 = new Content("3", "body2", "vi");
+        Content post4 = new Content("4", "body1", "vi");
         blog.addPost(post3);
         blog.addPost(post4);
-//        blog.addPost(new Content("title2", "body", "host"));
 
-//        post4.setReaction(Reaction.Type.ANGRY, "123");
 
-//        post4.getReactions().forEach(e-> System.out.println(e.getUserId()));
 
-//        blog.getBlogAuthorContents("host").forEach(e -> System.out.println(e.getBody()));
-//        blog.getAllContents().forEach(e -> {
-//            System.out.print(e.getTitle());
-//            System.out.print("_");
-//            System.out.println(e.getBody());
-//        });
+        System.out.println("-----------DESCENDINGPOST");
+        blog.getSortContents(BlogHost.SortType.DESCENDINGPOST).forEach(e -> System.out.println(e.getTitle()));
+        System.out.println("-----------ASCENDINGPOST");
+        blog.getSortContents(BlogHost.SortType.ASCENDINGPOST).forEach(e -> System.out.println(e.getTitle()));
 
+        post.modifyPostTitle("10");
+        post2.modifyPostTitle("11");
+        System.out.println("-----------DESCENDINGMODIFY");
+        blog.getSortContents(BlogHost.SortType.DESCENDINGMODIFY).forEach(e -> System.out.println(e.getTitle()));
+        System.out.println("-----------ASCENDINGMODIFY");
+        blog.getSortContents(BlogHost.SortType.ASCENDINGMODIFY).forEach(e -> System.out.println(e.getTitle()));
+        System.out.println("-----------ASCENDINGTITLE");
         blog.getSortContents(BlogHost.SortType.ASCENDINGTITLE).forEach(e -> System.out.println(e.getTitle()));
+
         Registry registry = new Registry();
         App app = new App(registry);
         registry.validate();
