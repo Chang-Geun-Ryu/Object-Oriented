69ac122fb09aca032237806b4db2dbb00517f893
ryu <goodrcg@gmail.com>
2020-06-09 11:14:17 +0900

    update lab6 25

--- Lab6/src/academy/pocu/comp2500/lab6/SetMenu.java
+++ Lab6/src/academy/pocu/comp2500/lab6/SetMenu.java
@@ -15,8 +15,7 @@ public class SetMenu extends Manu {
         } else if (super.price == MenuPrice.NO_HEAVY_MEAL) {
             assert (this.mainCourse != null) : "call isValid() first!";
             return new ArrayList<MainCourse>();
-        } else if (super.price == MenuPrice.THREE_COURSE_MEAL){.
-
+        } else if (super.price == MenuPrice.THREE_COURSE_MEAL){
             assert (this.mainCourse != null) : "call isValid() first!";
             return new ArrayList<MainCourse>(Collections.singleton(this.mainCourse));
         } else {
