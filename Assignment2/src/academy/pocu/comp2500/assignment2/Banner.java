d3cdf3ce9928e99c1996d53bbfdd671cd400c426
ryu <goodrcg@gmail.com>
2020-06-10 17:00:43 +0900

    update ass2 26

--- Assignment2/src/academy/pocu/comp2500/assignment2/Banner.java
+++ Assignment2/src/academy/pocu/comp2500/assignment2/Banner.java
@@ -5,13 +5,13 @@ import java.util.ArrayList;
 public class Banner extends Option {
 //    private BannerSize size;
     private int color;
-    private BannerType type;
+//    private BannerType type;
 
     protected Banner(BannerType type, BannerSize size, int color, OrientationType orientation, ShippingOptionsType deliveryMethod) {
         super(orientation);
 //        this.size = size;
         this.color = color;
-        this.type = type;
+//        this.type = type;
         setName(type.getName() + size.getSize());
         setPrice(size.getPrice() + type.getAddPrice());
         setShippingOptions(deliveryMethod);
@@ -19,9 +19,9 @@ public class Banner extends Option {
         setHeight(size.getHeight());
     }
 
-    public BannerType getType() {
-        return this.type;
-    }
+//    public BannerType getType() {
+//        return this.type;
+//    }
 
 //    public BannerSize getSize() {
 //        return this.size;
