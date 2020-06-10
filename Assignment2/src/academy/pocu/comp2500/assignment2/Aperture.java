70bfad55909b5f2bc8fcc3e71db553bd3f54d343
ryu <goodrcg@gmail.com>
2020-06-10 13:46:29 +0900

    update ass2 16

--- Assignment2/src/academy/pocu/comp2500/assignment2/Aperture.java
+++ Assignment2/src/academy/pocu/comp2500/assignment2/Aperture.java
@@ -1,19 +1,23 @@
 package academy.pocu.comp2500.assignment2;
 
 public class Aperture {
-    private String text;
+    private String element;
     private int x;
     private int y;
 //    private int width;
 //    private int height;
 //    private boolean sides;
 
-    public Aperture(String text, int x, int y) {
-        this.text = text;
+    public Aperture(String element, int x, int y) {
+        this.element = element;
         this.x = x;
         this.y = y;
 //        this.width = width;
 //        this.height = height;
+    }
+
+    public String getElemnet() {
+        return this.element;
     }
 
     public int getX() {
