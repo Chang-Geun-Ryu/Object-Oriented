d17d91d755f6057613729a19b97e9db391c59807
Chang-Geun-Ryu <goodrcg@gmail.com>
2020-06-10 23:42:21 +0900

    update ass2 109

--- Assignment2/src/academy/pocu/comp2500/assignment2/BannerLandscape.java
+++ Assignment2/src/academy/pocu/comp2500/assignment2/BannerLandscape.java
@@ -1,6 +1,6 @@
 package academy.pocu.comp2500.assignment2;
 
-public class BannerLandscape extends Banner {
+public class BannerLandscape extends OrientationBanner {
     private BannerType type;
     public BannerLandscape(BannerType type, BannerSize size, int color, ShippingOptionsType deliveryMethod) {
         super(type, size, color, OrientationType.LANDSCAPE, deliveryMethod);
