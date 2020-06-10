7458cd0bf89e38d8455cc5442f5c53f2539ec080
Chang-Geun-Ryu <goodrcg@gmail.com>
2020-06-10 23:40:20 +0900

    update ass2 108

--- Assignment2/src/academy/pocu/comp2500/assignment2/BannerPortrait.java
+++ Assignment2/src/academy/pocu/comp2500/assignment2/BannerPortrait.java
@@ -1,6 +1,6 @@
 package academy.pocu.comp2500.assignment2;
 
-public class BannerPortrait extends Banner {
+public class BannerPortrait extends OrientationBanner {
     private BannerType type;
     public BannerPortrait(BannerType type, BannerSize size, int color, ShippingOptionsType deliveryMethod) {
         super(type, size, color, OrientationType.PORTRAIT, deliveryMethod);
