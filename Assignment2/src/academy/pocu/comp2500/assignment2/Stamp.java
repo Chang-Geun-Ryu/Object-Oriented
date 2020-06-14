4c2222fa9f2abba2b03c76317c6abef6322806c9
Chang-Geun-Ryu <goodrcg@gmail.com>
2020-06-14 21:23:18 +0900

    update ass2 153

--- Assignment2/src/academy/pocu/comp2500/assignment2/Stamp.java
+++ Assignment2/src/academy/pocu/comp2500/assignment2/Stamp.java
@@ -1,30 +1,83 @@
+//package academy.pocu.comp2500.assignment2;
+//
+//public class Stamp extends Product {
+//    private String text;
+//    private StempColorType color;
+//
+//    public Stamp(String text, StempColorType color, StempSizeType stampSize, ShippingOptionsType deliveryMethod) {
+//        super();
+//
+//        this.color = color;
+////        setColor(stampColor.getColor());
+//
+//        if (stampSize == null) {
+//            setName("Stamp" + StempSizeType.FOUR_BY_THREE.getSize());
+//            setPrice(StempSizeType.FOUR_BY_THREE.getPrice());
+////            this.size = StempSizeType.FOUR_BY_THREE;
+//        } else {
+//            setName("Stamp" + stampSize.getSize());
+//            setPrice(stampSize.getPrice());
+////            this.size = stampSize;
+//        }
+//
+////        if (deliveryMethod == null) {
+////            setShippingOptions(ShippingOptionsType.PICKUP);
+////        } else {
+////            setShippingOptions(deliveryMethod);
+////        }
+//
+//        if (text == null) {
+//            this.text = "";
+//        } else {
+//            this.text = text;
+//        }
+//
+//        setWidth(stampSize.getWidth());
+//        setHeight(stampSize.getHeight());
+//    }
+//
+////    public void setStampSize(StempSizeType stampSize) {
+////        setWidth(stampSize.getWidth());
+////        setHeight(stampSize.getHeight());
+////    }
+////    public void setText(String text) {
+////        this.text = text;
+////    }
+//
+//    public StempColorType getStampColor() {
+//         return this.color;
+//    }
+//
+//    public String getText() {
+//        return this.text;
+//    }
+//
+//
+//}
+//
 package academy.pocu.comp2500.assignment2;
 
 public class Stamp extends Product {
     private String text;
-    private StempColorType color;
+    private StempColorType stampColor;
 
-    public Stamp(String text, StempColorType color, StempSizeType stampSize, ShippingOptionsType deliveryMethod) {
+    protected Stamp(String text, StempColorType stampColor, StempSizeType stampSize, ShippingOptionsType deliveryMethod) {
         super();
-
-        this.color = color;
 //        setColor(stampColor.getColor());
 
         if (stampSize == null) {
             setName("Stamp" + StempSizeType.FOUR_BY_THREE.getSize());
             setPrice(StempSizeType.FOUR_BY_THREE.getPrice());
-//            this.size = StempSizeType.FOUR_BY_THREE;
         } else {
             setName("Stamp" + stampSize.getSize());
             setPrice(stampSize.getPrice());
-//            this.size = stampSize;
         }
 
-//        if (deliveryMethod == null) {
-//            setShippingOptions(ShippingOptionsType.PICKUP);
-//        } else {
-//            setShippingOptions(deliveryMethod);
-//        }
+        if (deliveryMethod == null) {
+            setShippingOptions(ShippingOptionsType.PICKUP);
+        } else {
+            setShippingOptions(deliveryMethod);
+        }
 
         if (text == null) {
             this.text = "";
@@ -36,22 +89,16 @@ public class Stamp extends Product {
         setHeight(stampSize.getHeight());
     }
 
-//    public void setStampSize(StempSizeType stampSize) {
-//        setWidth(stampSize.getWidth());
-//        setHeight(stampSize.getHeight());
-//    }
-//    public void setText(String text) {
-//        this.text = text;
-//    }
-
-    public StempColorType getStampColor() {
-         return this.color;
+    public void setText(String text) {
+        this.text = text;
     }
 
     public String getText() {
         return this.text;
     }
 
-
+    public StempColorType getStampColor() {
+        return this.stampColor;
+    }
 }
 
