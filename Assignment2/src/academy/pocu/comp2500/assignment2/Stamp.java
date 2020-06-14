b26939d4c268db5fff60e938b44b40afba8a05a5
Chang-Geun-Ryu <goodrcg@gmail.com>
2020-06-14 23:08:14 +0900

    update ass2 156

--- Assignment2/src/academy/pocu/comp2500/assignment2/Stamp.java
+++ Assignment2/src/academy/pocu/comp2500/assignment2/Stamp.java
@@ -9,50 +9,15 @@ public class Stamp extends Product {
 
         this.color = color;
 
-        if (stampSize == null) {
-            setName("Stamp" + StempSizeType.FOUR_BY_THREE.getSize());
-            setPrice(StempSizeType.FOUR_BY_THREE.getPrice());
-//            this.size = StempSizeType.FOUR_BY_THREE;
-        } else {
-            setName("Stamp" + stampSize.getSize());
-            setPrice(stampSize.getPrice());
-//            this.size = stampSize;
-        }
+        setName("Stamp" + stampSize.getSize());
+        setPrice(stampSize.getPrice());
 
-        if (deliveryMethod == null) {
-            setShippingOptions(ShippingOptionsType.PICKUP);
-        } else {
-            setShippingOptions(deliveryMethod);
-        }
+        setShippingOptions(deliveryMethod);
 
-//        if (text == null) {
-//            this.text = "";
-//        } else {
-            this.text = text;
-//        }
+        this.text = text;
 
         setWidth(stampSize.getWidth());
         setHeight(stampSize.getHeight());
-    }
-
-//    public void setStampSize(StempSizeType stampSize) {
-//        setWidth(stampSize.getWidth());
-//        setHeight(stampSize.getHeight());
-//    }
-//    public void setText(String text) {
-//        this.text = text;
-//    }
-
-//    public StempColorType getStampColor() {
-//         return this.color;
-//    }
-//
-//    public String getText() {
-//        return this.text;
-//    }
-
-    public void setText(String text) {
-        this.text = text;
     }
 
     public String getText() {
@@ -64,50 +29,3 @@ public class Stamp extends Product {
     }
 
 }
-
-//package academy.pocu.comp2500.assignment2;
-//
-//public class Stamp extends Product {
-//    private String text;
-//    private StempColorType stampColor;
-//
-//    protected Stamp(String text, StempColorType stampColor, StempSizeType stampSize, ShippingOptionsType deliveryMethod) {
-//        super();
-//        this.stampColor = stampColor;
-//        if (stampSize == null) {
-//            setName("Stamp" + StempSizeType.FOUR_BY_THREE.getSize());
-//            setPrice(StempSizeType.FOUR_BY_THREE.getPrice());
-//        } else {
-//            setName("Stamp" + stampSize.getSize());
-//            setPrice(stampSize.getPrice());
-//        }
-//
-//        if (deliveryMethod == null) {
-//            setShippingOptions(ShippingOptionsType.PICKUP);
-//        } else {
-//            setShippingOptions(deliveryMethod);
-//        }
-//
-//        if (text == null) {
-//            this.text = "";
-//        } else {
-//            this.text = text;
-//        }
-//
-//        setWidth(stampSize.getWidth());
-//        setHeight(stampSize.getHeight());
-//    }
-//
-//    public void setText(String text) {
-//        this.text = text;
-//    }
-//
-//    public String getText() {
-//        return this.text;
-//    }
-//
-//    public StempColorType getStampColor() {
-//        return this.stampColor;
-//    }
-//}
-//
