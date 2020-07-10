af4c04b0e0a3c6f965dd2f324aebb2e8f861cbcb
ryu <goodrcg@gmail.com>
2020-07-10 17:05:26 +0900

    update ass3 1

--- Assignment3/src/academy/pocu/comp2500/assignment3/AttackIntent.java
+++ Assignment3/src/academy/pocu/comp2500/assignment3/AttackIntent.java
@@ -1,5 +1,7 @@
 package academy.pocu.comp2500.assignment3;
 
+import java.util.Objects;
+
 public class AttackIntent {
     private IntVector2D vector2D;
     private int damage;
@@ -21,5 +23,10 @@ public class AttackIntent {
 
     public Unit getAttacker() {
         return this.attacker;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(vector2D, damage, attacker);
     }
 }
