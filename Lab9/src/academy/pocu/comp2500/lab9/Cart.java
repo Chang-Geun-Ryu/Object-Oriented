b644168c3c0c1e270b673a6d311136333c3debf1
ryu <goodrcg@gmail.com>
2020-07-14 08:53:54 +0900

    update lab9 1

--- Lab9/src/academy/pocu/comp2500/lab9/Cart.java
+++ Lab9/src/academy/pocu/comp2500/lab9/Cart.java
@@ -5,6 +5,50 @@ import java.util.UUID;
 
 public final class Cart {
     private ArrayList<Book> books = new ArrayList<>();
+
+    public Book getBookOrNull(int index) {
+        if (this.books.size() <= index) {
+            return null;
+        }
+
+        return this.books.get(index);
+    }
+
+    public int getBookCount() {
+        return this.books.size();
+    }
+
+    public void addBooks(final ArrayList<Book> books) {
+        for (Book b : books) {
+            addBook(b);
+        }
+    }
+
+    public void addBook(Book book) {
+        this.books.add(book);
+    }
+
+    public boolean remove(int index) {
+        if (this.books.size() <= index) {
+            return false;
+        }
+
+        this.books.remove(index);
+
+        return true;
+    }
+
+    public int getTotalPrice() {
+        return new SimplePricing().getTotalPrice(this.books);
+    }
+
+    public int getTotalPrice(IPriceable pricingModel) {
+        return pricingModel.getTotalPrice(this.books);
+    }
+}
+/*
+public final class Cart {
+    private ArrayList<Book> books = new ArrayList<>();
     private IPriceable pricingModel;
 
     public void setPricing(IPriceable pricingModel) {
@@ -23,40 +67,13 @@ public final class Cart {
         return this.books.size();
     }
 
-//    public boolean addBooks(final UUID[] skus, final String[] titles, final int[] prices, final int[] publishedYears) {
-//        if (skus.length != titles.length || skus.length != prices.length || skus.length != publishedYears.length) {
-//            return false;
-//        }
-//
-//        for (int i = 0; i < skus.length; ++i) {
-//            Book book = new Book(skus[i], titles[i], prices[i], publishedYears[i]);
-//            this.books.add(book);
-//        }
-//
-//        return true;
-//    }
-
     public void addBooks(final ArrayList<Book> books) {
-//        if (skus.length != titles.length || skus.length != prices.length || skus.length != publishedYears.length) {
-//            return false;
-//        }
-
-//        for (int i = 0; i < skus.length; ++i) {
-//            Book book = new Book(skus[i], titles[i], prices[i], publishedYears[i]);
-//            this.books.add(book);
-//        }
         for (Book b : books) {
             addBook(b);
         }
     }
 
-//    public void addBook(final UUID sku, final String title, final int price, final int publishedYear) {
-//        Book book = new Book(sku, title, price, publishedYear);
-//        this.books.add(book);
-//    }
-
     public void addBook(Book book) {
-//        Book book = new Book(sku, title, price, publishedYear);
         this.books.add(book);
     }
 
@@ -78,4 +95,4 @@ public final class Cart {
             return this.pricingModel.getTotalPrice(this.books);
         }
     }
-}
+}*/
