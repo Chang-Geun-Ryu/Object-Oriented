b7a7d8d59835672eeaa0acc6e81b371abff03949
ryu <goodrcg@gmail.com>
2020-07-22 09:02:14 +0900

    update lab10 1

--- Lab10/src/academy/pocu/comp2500/lab10/MovieStore.java
+++ Lab10/src/academy/pocu/comp2500/lab10/MovieStore.java
@@ -7,14 +7,19 @@ import academy.pocu.comp2500.lab10.pocuflix.OkResult;
 import academy.pocu.comp2500.lab10.pocuflix.ResultBase;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 
 public class MovieStore implements IRequestHandler {
     private ArrayList<Movie> store = new ArrayList<>();
+//    private HashMap<Integer, Movie> store = new HashMap<>();
 
     public void add(Movie movie) {
-        if (store.contains(movie) == false) {
-            store.add(movie);
+        if (movie == null) {
+            return;
         }
+
+        store.add(movie);
     }
     
     public boolean remove(int index) {
@@ -26,7 +31,12 @@ public class MovieStore implements IRequestHandler {
 
     @Override
     public ResultBase handle(Request request) {
+        if (request == null || request.getTitle() == null) {
+            return new NotFoundResult();
+        }
+
         Movie movie = getMovie(request.getTitle());
+
         if (movie != null) {
             return new OkResult(movie);
         } else {
