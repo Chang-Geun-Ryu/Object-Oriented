dba7310886cc13132dcb0e1707d7299cbe1a8a45
ryu <goodrcg@gmail.com>
2020-07-22 12:20:34 +0900

    update ass4

--- Assignment4/src/academy/pocu/comp2500/assignment4/OverdrawAnalyzer.java
+++ Assignment4/src/academy/pocu/comp2500/assignment4/OverdrawAnalyzer.java
@@ -1,4 +1,27 @@
 package academy.pocu.comp2500.assignment4;
 
-public class OverdrawAnalyzer {
+import java.util.LinkedList;
+
+public class OverdrawAnalyzer extends Canvas {
+    public OverdrawAnalyzer(int width, int height) {
+        super(width, height);
+    }
+
+    public LinkedList<Character> getPixelHistory(int x, int y) {
+        LinkedList<Character> list = new LinkedList<Character>();
+
+
+        return list;
+    }
+
+    public int getOverdrawCount(int x, int y) {
+
+
+        return 0;
+    }
+
+    public int getOverdrawCount() {
+
+        return 0;
+    }
 }
