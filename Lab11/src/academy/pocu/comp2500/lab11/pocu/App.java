c8e423964ddce55edc777f94753d067f94ba7dd3
ryu <goodrcg@gmail.com>
2020-07-27 16:46:33 +0900

    update lab11

---
+++ Lab11/src/academy/pocu/comp2500/lab11/pocu/App.java
@@ -0,0 +1,42 @@
+package academy.pocu.comp2500.lab11.pocu;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.PrintStream;
+
+public class App {
+
+    public void run(BufferedReader in, PrintStream out, PrintStream err) {
+        out.println("WAREHOUSE: Choose your warehouse!");
+        out.println("1. APPLE");
+        out.println("2. MICROSOFT");
+        out.println("3. SAMSUNG");
+
+        try {
+            String s = in.readLine();
+
+            if (s != null) {
+
+            }
+
+            if (s.length() >= 0) {
+
+            }
+
+            if (s.length() > 1) {
+
+            }
+
+
+        } catch (IOException e) {
+
+        } catch (Exception e) {
+
+        }
+
+
+
+
+
+    }
+}
